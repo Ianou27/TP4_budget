e9e8d02342caf41df71471ef97a1c5156d166142
ianou27 <alexis.ian23@gmail.com>
2023-10-31 21:58:35 -0400

    fixed myscript.py

--- myscript.py
+++ myscript.py
@@ -4,5 +4,5 @@ badhash = "c1a4be04b972b6c17db242fc37752ad517c29402"
 goodhash = "84cae48c4a7a261844abf94cc9925b90605fda00"
 
 os.system(f'git bisect start {badhash} {goodhash}')
-os.system('git bisect run python3.10 manage.py test')
+os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
